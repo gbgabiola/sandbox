ca16dd02734543e4cc56250bb69d85120bd89b00
Genesis Gabiola <genesisbritanicogabiola@gmail.com>
2019-10-03 15:16:45 +0800

    Add a calculate & error functionality

---
+++ loan-calculator/app.js
@@ -0,0 +1,59 @@
+// Listen for submit
+document
+  .querySelector('#loan-form')
+  .addEventListener('submit', calculateResults);
+
+function calculateResults(e) {
+  console.log('Calculating...');
+  // UI vars
+  const amount = document.querySelector('#amount');
+  const interest = document.querySelector('#interest');
+  const years = document.querySelector('#years');
+  const monthlyPayment = document.querySelector('#monthly-payment');
+  const totalPayment = document.querySelector('#total-payment');
+  const totalInterest = document.querySelector('#total-interest');
+
+  const principal = parseFloat(amount.value);
+  const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+  const calculatePayments = parseFloat(years.value) * 12;
+
+  // Compute monthly payment
+  const x = Math.pow(1 + calculatedInterest, calculatePayments);
+  const monthly = (principal * x * calculatedInterest) / (x - 1);
+
+  if (isFinite(monthly)) {
+    monthlyPayment.value = monthly.toFixed(2);
+    totalPayment.value = (monthly * calculatePayments).toFixed(2);
+    totalInterest.value = (monthly * calculatePayments - principal).toFixed(2);
+  } else {
+    // console.log('Please check your inputs');
+    showError('Please check your inputs...');
+  }
+
+  e.preventDefault();
+}
+
+// Show error
+function showError(error) {
+  const errorDiv = document.createElement('div');
+
+  // Get elements
+  const card = document.querySelector('.card');
+  const heading = document.querySelector('.heading');
+
+  // Add class
+  errorDiv.className = 'alert alert-danger';
+
+  // Create text node and append to div
+  errorDiv.appendChild(document.createTextNode(error));
+
+  // Insert error above heading
+  card.insertBefore(errorDiv, heading);
+
+  // Clear error after 3 seconds
+  setTimeout(clearError, 3000);
+}
+
+function clearError() {
+  document.querySelector('.alert').remove();
+}
